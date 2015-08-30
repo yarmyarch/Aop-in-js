cc1944f0a09ba3322853b9c1f7687d0362d023e8
Yujia Luo <yarmyarch@live.cn>
2015-08-29 20:10:23 -0700

    Updated using obj as param.

--- AopUtil.js
+++ AopUtil.js
@@ -1,115 +1,151 @@
-var AopUtil = {};
-	
-(function() {
-	
-AopUtil.ALLOW_IN = 1;
+var AopUtil = (function() {
 
-AopUtil.ALLOW_OUT = 2;
-	
-//QuitAction处理 当接收到一个return为Action_quit时，强制中断后续所有的Aspect
-AopUtil.ACTION_QUIT = "Aop_" + Math.floor(Math.random()*1000000).toString(36);
-	
-var original = {};
-	
-var beforeAdviceCounter = {};
-	
-var createProxyMethod = function(originalItem) {
-	return function() {
-		
-		var currentArg = arguments;
-		var currentReturn = arguments;
-		var lastReturn = arguments;
-		
-		//计算组合策略（参数+返回值策略）中的参数策略
-		//isInAllowed为策略值二进制表示中的最低位值，isOutAllowed为次低位，isQuit为第三位
-		var isInAllowed, isOutAllowed, isQuit;
-		
-		var advice, method, fromOriginal = 0;
-		
-		for (var i in originalItem.adviceChain) {
-			
-            advice = originalItem.adviceChain[i];
-            method = advice.method;
-            
-			//读取策略组
-			isInAllowed = advice.strategy;				
-			isOutAllowed = isInAllowed >> 1;
-			
-			isInAllowed = isInAllowed - (isInAllowed >> 1 << 1);
-			isOutAllowed = isOutAllowed - (isOutAllowed >> 1 << 1);
-			
-			if (isInAllowed) currentArg = lastReturn;
-			else currentArg = arguments;
-            
-            if (fromOriginal) {
-                currentArg = [currentArg];
-                fromOriginal = 0;
-            }
-            
-			currentReturn = method.apply(this, currentArg);
-            
-            // if it's returned from the original function, give it a wrap to prevent error.
-            if (method == originalItem.backup) {
-                fromOriginal = 1;
-            }
-			
-			//Action处理
-			if (currentReturn == AopUtil.ACTION_QUIT) break;
-			
-			if (isOutAllowed) lastReturn = currentReturn;
-		}
-		return lastReturn;
-	}
-}
-	
-var attachToAop = function(methodName, strategy) {
-	
-	//忽略重复校验，因为如下场景会导致问题：
-	//  对iframe中某方法添加aop；
-	//  iframe改变src，需要重新添加aop，此时则会添加失败。
-	//~ if (original[methodName])  {
-		//~ return null;
-	//~ }
-	var sourceMethod = eval(methodName);
-	if (!sourceMethod) return null;
-	
-	beforeAdviceCounter[methodName] = 0;
-	//初始化adviceChain
-	original[methodName] = {};
-	original[methodName].backup = sourceMethod;
-	original[methodName].adviceChain = [{method : sourceMethod, strategy : strategy}];
-	eval(methodName  + " = createProxyMethod(original[methodName])");
-}
+  var self;
+    
+  var aspects = {};
+  
+  /**
+   * Gets the index for an object within all aspects that's having the same methodName.
+   * @return -1 if not found, otherwise the index starts from 0.
+   */
+  var getAspectIndex = function(obj, methodName) {
+    if (!aspects[methodName] || !aspects[methodName].length) {
+      return -1;
+    }
+    for (var i = 0, aspect; aspect = aspects[methodName][i]; ++i) {
+      if (aspect.obj == obj) {
+        return i;
+      }
+    }
+    return -1;
+  };
+    
+  var createProxyMethod = function(originalItem) {
+    return function() {
+      
+      var currentArg = arguments;
+      var currentReturn = arguments;
+      var lastReturn = arguments;
+      
+      //计算组合策略（参数+返回值策略）中的参数策略
+      //isInAllowed为策略值二进制表示中的最低位值，isOutAllowed为次低位，isQuit为第三位
+      var isInAllowed, isOutAllowed;
+      
+      var advice, method, fromOriginal = 0;
+      
+      for (var i in originalItem.adviceChain) {
+        
+        advice = originalItem.adviceChain[i];
+        method = advice.method;
+        
+        //读取策略组
+        isInAllowed = advice.strategy;        
+        isOutAllowed = isInAllowed >> 1;
+        
+        isInAllowed = isInAllowed - (isInAllowed >> 1 << 1);
+        isOutAllowed = isOutAllowed - (isOutAllowed >> 1 << 1);
+        
+        if (isInAllowed) {
+          currentArg = lastReturn;
+        } else {
+          currentArg = arguments;
+        }
+        if (fromOriginal) {
+          currentArg = [currentArg];
+          fromOriginal = 0;
+        }
+        
+        currentReturn = method.apply(this, currentArg);
+        
+        // if it's returned from the original function, give it a wrap to prevent error.
+        if (method == originalItem.backup) {
+          fromOriginal = 1;
+        }
+        
+        //Action处理
+        if (currentReturn == self.ACTION_QUIT) {
+          break;
+        }
+        
+        if (isOutAllowed) {
+          lastReturn = currentReturn;
+        }
+      }
+      return lastReturn;
+    };
+  };
+    
+  var attachToAop = function(obj, methodName, strategy) {
+    var sourceMethod = obj[methodName];
+    if (!sourceMethod) {
+      return;
+    }
+    
+    //初始化adviceChain
+    var aspect = {};
+    aspect.obj = obj;
+    aspect.beforeCount = 0;
+    aspect.backup = sourceMethod;
+    aspect.adviceChain = [{method : sourceMethod, strategy : strategy}];
+    
+    aspects[methodName] = aspects[methodName] || [];
+    aspects[methodName].push(aspect);
+    obj[methodName] = createProxyMethod(aspect);
+    
+    return aspect;
+  };
 
-AopUtil.before = function(methodName, command, strategy) {
-	
-	if (!original[methodName] || original[methodName].backup != eval('(' + methodName + ')')) attachToAop(methodName, 3);
-	original[methodName].adviceChain.splice(
-		beforeAdviceCounter[methodName], 0, {
-		method : command,
-		//默认以原始参数输入，不允许输出
-		strategy : (strategy ? strategy : 0)
-	});
-	++beforeAdviceCounter[methodName];
-}
+  return self = {
+    ALLOW_IN: 1,
+    ALLOW_OUT: 2,
+    //QuitAction处理 当接收到一个return为Action_quit时，强制中断后续所有的Aspect
+    ACTION_QUIT: "Aop_" + Math.floor(Math.random()*1000000).toString(36),
+    
+    before: function(obj, methodName, command, strategy) {
+      
+      aspects[methodName] = aspects[methodName] || [];
+      var objIndex = getAspectIndex(obj, methodName),
+          aspect = aspects[methodName][objIndex];
+      
+      if (objIndex == -1) {
+        aspect = attachToAop(obj, methodName, 3);
+      }
+      aspect.adviceChain.splice(
+        aspect.beforeCount, 0, {
+        method : command,
+        //默认以原始参数输入，不允许输出
+        strategy : (strategy ? strategy : 0)
+      });
+      ++aspect.beforeCount;
+    },
 
-AopUtil.after = function(methodName, command, strategy) {
-	
-	//原始函数两端全开
-	if (!original[methodName] || original[methodName].backup != eval('(' + methodName + ')')) attachToAop(methodName, 3);
-	original[methodName].adviceChain.push({
-		method : command,
-		//默认以原始参数输入，不允许输出
-		strategy : (strategy ? strategy : 0)
-	});
-}
+    after: function(obj, methodName, command, strategy) {
+      
+      aspects[methodName] = aspects[methodName] || [];
+      var objIndex = getAspectIndex(obj, methodName),
+          aspect = aspects[methodName][objIndex];
+      
+      //原始函数两端全开
+      if (objIndex == -1) {
+        aspect = attachToAop(obj, methodName, 3);
+      }
+      aspect.adviceChain.push({
+        method : command,
+        //默认以原始参数输入，不允许输出
+        strategy : (strategy ? strategy : 0)
+      });
+    },
 
-AopUtil.clearAdvice = function(methodName) {
-	
-	if (original[methodName]) {
-		eval(methodName + "= original[methodName].backup");
-		original[methodName] = null;
-	}
-}
+    clearAdvice: function(obj, methodName) {
+      
+      var objIndex = getAspectIndex(obj, methodName),
+          aspect = aspects[methodName][objIndex];
+      
+      if (objIndex != -1) {
+        obj[methodName] = aspect.backup;
+        aspects[methodName].splice(objIndex, 1);
+      }
+    }
+  };
 
 })();
